481a4aadc849b2d3d42b540a1ed11926220656e6
rajyalakshmi <rajianna239@gmail.com>
2023-04-12 20:49:44 +0530

    first commit

--- app/views.py
+++ app/views.py
@@ -18,9 +18,6 @@ def display_Webpage(request):
     LOW=Webpage.objects.filter(name__endswith='raj')
     LOW=Webpage.objects.all()
     LOW=Webpage.objects.filter(Q(name='dhoni')& Q(mail='dhonigmail.com'))
-    LOW=Webpage.objects.filter(name__endswith='raj')
-    LOW=Webpage.objects.filter(name__contains='h')
-    LOW=Webpage.objects.filter(name__regex='[a-zA-Z]{5}')
     d={'topics':LOW}
     return render(request,'display_Webpage.html',d)
 def Access(request):
